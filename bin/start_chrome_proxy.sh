af3c5d4e6acffed8ffdf4f64a30679d360c97699
cthomas <cthomas@navigationsolutions.com>
2014-03-07 14:51:34 -0500

    Updating start_chrome_proxy.sh to support chromium/google-chrome

--- bin/start_chrome_proxy.sh
+++ bin/start_chrome_proxy.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-google-chrome --proxy-server="socks5://localhost:9999"
+BROWSER_BIN=""
+
+if [ -n "`which google-chrome`" ]
+then
+	BROWSER_BIN="google-chrome"
+elif [ -n "`which chromium-browser`" ]
+then
+	BROWSER_BIN="chromium-browser"
+fi
+
+if [ -n "$BROWSER_BIN"  ]
+then
+	$BROWSER_BIN --proxy-server="socks5://localhost:9999"
+else
+	echo "Could not find Chrome!"
+fi
